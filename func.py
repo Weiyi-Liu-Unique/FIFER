cdf448c3ba6dc18b2c2bbaadae296a841775797b
Weiyi-Liu-Unique <weiyi_liu1@outlook.com>
2023-08-01 11:07:44 +0800

    update-2023.8.1

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+import os
 import time
 import cv2
 import numpy as np
@@ -28,17 +29,36 @@ def LoadData(path: str):
     return ImgSet
 
 
-def MotionCorrection(LoadPath, SavePath=None):
-    so = ct.cdll.LoadLibrary("FIFER_LIB.dll")
+def MotionCorrection(LoadPath: str, SavePath: str, Flag = 1, IfRigid = False):
+    """
+    Flag: int, default: 1
+        Flag == 0: set first frame as template image
+        Flag == 1: set average of registered random 20 frames as template image
+        Flag == 2: set average of registered first 20 frames as template image  
+    IfRigid: bool, default: False
+        IfRigid == True: Translation + Rotation (low efficiency)
+        IfRigid == False: Translation (recommanded)
+    """
+
+    # Load model
+    script_path=os.path.abspath(__file__)
+    dir_path=os.path.dirname(script_path)
+    os.add_dll_directory(dir_path)
+    so = ct.cdll.LoadLibrary("./FIFER.dll")
+
+    # Load video and set vars
     InputStack = LoadData(LoadPath)
     OutputStack = np.zeros(InputStack.shape, dtype=np.uint8)
     pInputStack = InputStack.ctypes.data_as(ct.POINTER(ct.c_uint8))
     pOutputStack = OutputStack.ctypes.data_as(ct.POINTER(ct.c_uint8))
     C, Rows, Cols = InputStack.shape
-    IfRigid = False
-    so.MotionCorrection(pInputStack, pOutputStack, C, Rows, Cols, IfRigid)
-    if SavePath is not None:
-        Write(SavePath, OutputStack, 20)
+
+    # Excute motion correction
+    so.MotionCorrection(pInputStack, pOutputStack, C, Rows, Cols, Flag, IfRigid)
+    
+    # save registered video
+    fps = 20  # set by yourself
+    Write(SavePath, OutputStack, fps)
 
 
 if __name__ == '__main__':
